f78fc38fd5743a1b76c3ab3824032550d74bf4a0
pedrogasparini <pedrogasparini99@gmail.com>
2025-04-10 19:43:40 -0300

    arreglo de async y avance en request, dtos y services (job)

--- PPS_Changas_API/Application/Interfaces/IJobService.cs
+++ PPS_Changas_API/Application/Interfaces/IJobService.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Application.Model;
+using Application.Model.Requests;
+using Domain.Entities;
 
 namespace Application.Interfaces
 {
     public interface IJobService
     {
+        // Task<List<JobsDTO>> GetAll();
+        // JobsDTO GetById(int id);
+        Job Create(JobRequest request);
+        // Job Update(JobUpdateRequest request);
+        // Job Delete (int id);
     }
 }
