20aa78c3da4513872425c936f97a3b6276b94213
MarucoMass <mariomass23@gmail.com>
2025-04-10 14:49:55 -0300

    actualicé entidades según el nuevo diagrama de clases

--- PPS_Changas_API/Domain/Entities/Job.cs
+++ PPS_Changas_API/Domain/Entities/Job.cs
@@ -9,14 +9,18 @@ namespace Domain.Entities
 {
     public class Job
     {
-        public Employer Employer { get; set; }
-        public List<Postulation> Postulations { get; set; }
         public string Title { get; set; }
-        public float Price { get; set; }
-        public string DateTime { get; set; }
+        public float AveragePrice { get; set; }
+        public DateTime DateTime { get; set; }
         public bool Available { get; set; }
         public string Location { get; set; }
         public string Description { get; set; }
-        public CategoryEnum CategoryEnum { get; set; }
+        public CategoryEnum Category { get; set; }
+
+        public Employer Employer { get; set; }
+        public int IdEmployer { get; set; }
+        public List<Postulation> Postulations { get; set; } = new();
+        public int AmountPostulations => Postulations.Count;
+
     }
 }
