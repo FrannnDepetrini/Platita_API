958b25dcd096557e19c00b8f7dc9b60ea5b4f33a
Joaquin Tanlongo <joako.tanlon@gmail.com>
2025-04-18 19:08:21 -0300

    Creacion de AuthController

--- PPS_Changas_API/Application/Interfaces/IAuthService.cs
+++ PPS_Changas_API/Application/Interfaces/IAuthService.cs
@@ -1,5 +1,4 @@
-﻿using Domain.Constants;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
---
+++ PPS_Changas_API/Web/Controllers/AuthController.cs
@@ -0,0 +1,47 @@
+﻿using Application.Interfaces;
+using Application.Model.Requests;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+        [HttpPost ("[action]")]
+        public async Task<IActionResult> Login(LoginRequest1 user)
+        {
+            try
+            {
+                var token = await _authService.Login(user.Email, user.Password);
+                return Ok(token);
+            }
+            catch (System.Exception)
+            {
+                return Unauthorized();
+            }
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Register([FromBody]RegisterRequest1 user)
+        {
+            try
+            {
+                await _authService.Register(User, user.Email, user.Password, user.Role, user.UserName, user.PhoneNumber);
+                return Ok("Usuario registrado");
+            }
+            catch (System.Exception)
+            {
+                return BadRequest("Usuario existente");
+            }
+        }
+    }
+}
