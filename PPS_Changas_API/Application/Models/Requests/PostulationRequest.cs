ebe10817fc09a8024cffdd0015a94ce9729e3889
MarucoMass <mariomass23@gmail.com>
2025-05-08 18:00:29 -0300

    creé los dtos para postulation. 1 para ver mis postulaciones y otro para que el publicador del trabajo vea las postulaciones del mismo

---
+++ PPS_Changas_API/Application/Models/Requests/PostulationRequest.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Constants;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Models.Requests;
+
+public class PostulationRequest
+{
+    public int JobId { get; set; }
+    public float Budget { get; set; }
+}
