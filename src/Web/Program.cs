f8d9389b02f4ac30e9cb1fdee78c7bbb6773dada
Pedro Ortiz Gasparini <pedrogasparini99@gmail.com>
2025-05-14 19:43:44 -0300

    creacion de update

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -86,6 +86,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IJobService, JobService>();
 builder.Services.AddScoped<ITokenService, TokenService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IClientService, ClientService>();
 builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
 builder.Services.AddScoped<IEmailService, EmailService>();
 builder.Services.AddScoped<IUserService, UserService> ();
